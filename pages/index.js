4cc9a298da2880b234d3f3a8ff6f684cfa555e06
AndySch666 <schmidt.hof@web.de>
2022-09-06 15:22:21 +0200

    add image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
+  <>
     <h1>Startseite</h1>
+    <Image 
+    alt="" 
+    src="https://images.unsplash.com/photo-1617470703128-26a0fc9af10f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80" 
+    layout="responsive" 
+    width={2400} 
+    height={1599} />
+  </>
   )
 }
